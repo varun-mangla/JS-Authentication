0f9e35158f4009e998d996aa6f651e1d136a3d3e
Varun Mangla <iamvarunmangla@gmail.com>
2025-03-15 01:00:29 +0530

    controller updated

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,5 +1,15 @@
 const registerUser = async (req,res)=>{
-    res.send ("registered")
+    // get data
+    // validate
+    // check is alredy exists
+    // create a user in database
+    // create a verification token
+    // save token in database
+    // email token to user
+    // success status to user
+
+
+
 };
 
 export {registerUser}
